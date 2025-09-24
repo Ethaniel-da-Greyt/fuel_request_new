e91446b744047961d274cbca69aa0fae8dc335fa
Ethaniel Obordo <ethaniel.obordo.a@gmail.com>
2025-09-24 21:42:02 +0800

    Login function added in AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,15 +5,18 @@ import {
   useContext,
   useEffect,
   useState,
+  useCallback
 } from "react";
 type AuthContextType = {
   //Nag create ta ug Type pra sa AuthContext
   token: string | null; // Dri nag declare ta sa token ug string or null(wala)
   setToken: (token: string | null) => void; //gi include pud ang setToken nga function nga ang sulod sa parameter is only token nga string
   logout: () => void; //last kay nag include ghapon sa logout function
+  login: (email: string, password: string) => void; //last kay nag include ghapon sa logout function
 };
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+const loginAPI = `${process.env.NEXT_PUBLIC_APP_URL}/login`;
 const LogoutLink = `${process.env.NEXT_PUBLIC_APP_URL}/logout`;
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
@@ -34,7 +37,36 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setTokenState(token);
   };
 
-  const logout = async () => {
+  const login = useCallback(async (email: string, password: string) => {
+    try {
+      const reslog = await fetch(loginAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({ email, password})
+      });
+
+      if (!reslog.ok) {
+        console.log("Error: ", reslog.status);
+      }
+
+      const data = await reslog.json();
+      if(data.access_token)
+      {
+        localStorage.setItem("token", data.access_token);
+        setToken(data.access_token);
+        route.replace("/admin/Display");
+      }
+
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  }, [route]);
+
+
+  const logout = useCallback(async () => {
     try {
       const getToken = localStorage.getItem("token");
       const res = await fetch(LogoutLink, {
@@ -60,10 +92,10 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setToken(null);
       route.replace("/");
     }
-  };
+  }, [route]);
 
   return (
-    <AuthContext.Provider value={{ token, setToken, logout }}>
+    <AuthContext.Provider value={{ token, setToken, logout, login }}>
       {children}
     </AuthContext.Provider>
   );
